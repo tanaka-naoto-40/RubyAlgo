27749f70afb8fc1fef88bb61b1862061cb220eba
tanakanaoto <na0to9.28@gmail.com>
2023-07-19 22:27:44 +0900

    modified:   config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,12 @@ module RubyAru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.generators do |g|
+      g.skip_routes true
+      g.assets false
+      g.helper false
+      g.test_framework false
+    end
     config.i18n.default_locale = :ja
     config.i18n.load_path += Dir[Rails.root.join('config/locales/**/*.{rb,yml}').to_s]
     config.active_record.default_timezone = :local
