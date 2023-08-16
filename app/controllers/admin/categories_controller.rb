00c5b34593c4035891605249a77853e66bd360f8
tanakanaoto <na0to9.28@gmail.com>
2023-08-16 15:28:54 +0900

    Admin,Course

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -15,9 +15,8 @@ class Admin::CategoriesController < Admin::BaseController
 
   def create
     @category = Category.new(category_params)
-
     if @category.save
-      redirect_to admin_categories_path
+      redirect_to admin_categories_path, notice: 'Categoryを作成しました'
     else
       render :new
     end
@@ -25,7 +24,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def update
     if @category.update(category_params)
-      redirect_to admin_categories_path
+      redirect_to admin_categories_path, notice: 'Categoryを更新しました'
     else
       render :edit
     end
@@ -33,7 +32,7 @@ class Admin::CategoriesController < Admin::BaseController
 
   def destroy
     @category.destroy!
-    redirect_to admin_category_path
+    redirect_to admin_category_path, notice: 'Categoryを削除しました'
   end
 
   private
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,6 @@
 class Category < ApplicationRecord
   has_many :courses
+
+  validates :title, presence: true
+  validates :content, presence: true
 end
