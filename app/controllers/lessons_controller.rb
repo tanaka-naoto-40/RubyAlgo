a2d9639ab2862552a6e02827bae95c391e33c0f4
tanakanaoto <na0to9.28@gmail.com>
2023-10-07 12:59:56 +0900

    [Fix]when Quiz answer nil

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -6,20 +6,50 @@ class LessonsController < ApplicationController
     @lessons = @course.lessons.where(published: true)
   end
 
+  # def result
+  #   @selected_answers = params[:answers]
+  #   @results = {}
+  #   @selected_answers&.each do |lesson_id, answer_id|
+  #     lesson = Lesson.find(lesson_id) # Find the lesson
+  #     answer = Answer.find(answer_id)
+  #     @results[lesson_id] = {
+  #       lesson_title:    lesson.title,
+  #       lesson_content:  lesson.content,
+  #       lesson_answers:  lesson.answers,
+  #       answer_id:       answer.id,
+  #       answer_content:  answer.content,
+  #       correct:         answer.answer
+  #     } # Keep the lesson's details and the correctness of the answer
+  #   end
+  # end
+
   def result
     @selected_answers = params[:answers]
     @results = {}
-    params[:answers]&.each do |lesson_id, answer_id|
+    @selected_answers&.each do |lesson_id, answer_id|
       lesson = Lesson.find(lesson_id) # Find the lesson
-      answer = Answer.find(answer_id)
-      @results[lesson_id] = {
-        lesson_title:    lesson.title,
-        lesson_content:  lesson.content,
-        lesson_answers:  lesson.answers,
-        answer_id:       answer.id,
-        answer_content:  answer.content,
-        correct:         answer.answer
-      } # Keep the lesson's details and the correctness of the answer
+  
+      if answer_id == "default"
+        # 未選択の場合の処理
+        @results[lesson_id] = {
+          lesson_title:    lesson.title,
+          lesson_content:  lesson.content,
+          lesson_answers:  lesson.answers,
+          correct:         false
+        }
+      else
+        # 選択された選択肢がある場合の処理
+        answer = Answer.find(answer_id)
+        @results[lesson_id] = {
+          lesson_title:    lesson.title,
+          lesson_content:  lesson.content,
+          lesson_answers:  lesson.answers,
+          answer_id:       answer.id,
+          answer_content:  answer.content,
+          correct:         answer.answer
+        }
+      end
     end
   end
+  
 end
