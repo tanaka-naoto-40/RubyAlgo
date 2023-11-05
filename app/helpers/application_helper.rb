113b4cd2e83c95bed63efbdd7730fc30814ee61f
tanakanaoto <na0to9.28@gmail.com>
2023-11-05 15:55:15 +0900

    [Add]OGP image

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -10,4 +10,35 @@ module ApplicationHelper
   def turbo_stream_flash
     turbo_stream.append "flashes", partial: "flash"
   end
+
+    def default_meta_tags
+      {
+        site: 'RubyAlgo',
+        title: '環境構築不要でRubyを学習できるサービス',
+        reverse: true,
+        charset: 'utf-8',
+        description: 'RubyAlgoを使い、Rubyの学習を行いましょう。',
+        keywords: 'Ruby, Ruby on Rails',
+        canonical: request.original_url,
+        separator: '|',
+        icon: [
+          { href: image_url('favicon.ico') },
+        ],
+        og: {
+          site_name: :site,
+          title: :title,
+          description: :description,
+          type: 'website',
+          url: request.original_url,
+          image: image_url('ogp.png'), # 配置するパスやファイル名によって変更すること
+          local: 'ja-JP'
+        },
+        # Twitter用の設定を個別で設定する
+        twitter: {
+          card: 'summary_large_image', # Twitterで表示する場合は大きいカードにする
+          image: image_url('ogp.png') # 配置するパスやファイル名によって変更すること
+        }
+      }
+    end
+
 end
